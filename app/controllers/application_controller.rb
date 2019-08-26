8168a7681b44d5616db08703cdce106b03044e46
tsuyoshit201603 <tsuyoshi.t201603@gmail.com>
2019-08-26 17:15:12 +0900

    usersテーブルにおいてストロングパラメーターの指定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -12,5 +13,10 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :cana, :birthday, :nicname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :cana, :birthday, :nicname])
   end
 end
