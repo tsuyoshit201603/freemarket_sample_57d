6d05c4361ffa519873d649bedf7499ba1424d734
tsuyoshit201603 <tsuyoshi.t201603@gmail.com>
2019-08-26 14:04:00 +0900

    テストで使用したスキーマファイルを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190825040839) do
-
-  create_table "tests", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "image",      null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
