e62c00461b614650230871d6522ca857e543715e
JUN1113 <dunkelrots@gmail.com>
2019-08-26 20:00:16 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  validates :first_name_kanji, presence: true
+  validates :family_name_kanji, presence: true
+  validates :first_name_cana, presence: true
+  validates :family_name_cana, presence: true
+  validates :year, presence: true
+  validates :month, presence: true
+  validates :day, presence: true
 end
