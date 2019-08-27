0a790c1fddae52507f4db837e86a3c1fd8d72234
tsuyoshit201603 <tsuyoshi.t201603@gmail.com>
2019-08-27 18:00:50 +0900

    facebookとの認証

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable
   validates :nickname, presence: true
   validates :first_name_kanji, presence: true
   validates :family_name_kanji, presence: true
@@ -11,4 +11,19 @@ class User < ApplicationRecord
   validates :year, presence: true
   validates :month, presence: true
   validates :day, presence: true
+
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+ 
+    unless user
+      user = User.create(
+        uid:      auth.uid,
+        provider: auth.provider,
+        email:    auth.info.email,
+        password: Devise.friendly_token[0, 20]
+      )
+    end
+ 
+    user
+  end
 end
