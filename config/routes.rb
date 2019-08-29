d3e41a4b65bc42e66b84d23ad3db2ec095a32be2
hashryo <ryo_hashimoto1108@icloud.com>
2019-08-29 20:10:53 +0900

    ルート修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
   root "products#index"
   resources :products
