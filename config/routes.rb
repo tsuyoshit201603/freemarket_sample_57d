46c8cae322632b42de7f4e6e81c35260dab76d40
hashryo <ryo_hashimoto1108@icloud.com>
2019-08-29 18:10:24 +0900

    ログアウト、サインアップ、サインイン、マイページへのリンク仮置き

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
   root "products#index"
   resources :products
+  resources :users, only: [:show]
 end
