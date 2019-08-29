45256f12676a500047356cb6dd5d3a5afe4ff56e
tsuyoshit201603 <tsuyoshi.t201603@gmail.com>
2019-08-29 20:37:02 +0900

    ルーテイングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
   root "products#index"
   resources :products
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :identifications, only: [:index]
+  end
 end
