8d71869bc5cfb0ec95d21f4b4245c6f9420ad519
JUN1113 <dunkelrots@gmail.com>
2019-08-31 16:13:55 +0900

    activeを指定

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  layout "devise"
+  before_action :set_active
   def create
     if params[:user][:password] == "" && request.referer.match("auth") != nil
       pass = Devise.friendly_token.first(7)
@@ -19,5 +21,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def after_inactive_sign_up_path_for(resource)
     new_telphone_path
+  end
+  private
+  def set_active
+    @active = ["","","",""]
   end
 end
