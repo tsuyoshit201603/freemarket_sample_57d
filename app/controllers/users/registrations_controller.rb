88fc42065588661bbb1634e55c51d927a077aa57
JUN1113 <dunkelrots@gmail.com>
2019-08-31 16:13:24 +0900

    行を調整

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,14 +2,14 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   def create
-     if params[:user][:password] == "" && request.referer.match("auth") != nil
-       pass = Devise.friendly_token.first(7)
-       params[:user][:password] = pass
-       params[:user][:password_confirmation] = pass
-       super
-       sns = SnsCredential.update(user_id:  @user.id)
-     else
-       super
-     end
-   end
+    if params[:user][:password] == "" && request.referer.match("auth") != nil
+      pass = Devise.friendly_token.first(7)
+      params[:user][:password] = pass
+      params[:user][:password_confirmation] = pass
+      super
+      sns = SnsCredential.update(user_id:  @user.id)
+    else
+      super
+    end
+  end
 end
