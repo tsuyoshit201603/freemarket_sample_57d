50f4699c3342b61428b0bf9b1484252abaa98883
JUN1113 <dunkelrots@gmail.com>
2019-09-05 13:26:40 +0900

    routingにpasswordsを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions', omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
+  devise_for :users, controllers: { sessions: 'users/sessions', omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations', passwords: "users/passwords" }
   root "products#index"
   resources :products do
     resources :buyings, only: :index
