14ee612a2c17c65a05dfdb738b7357d4ea7f2303
tsuyoshit201603 <tsuyoshi.t201603@gmail.com>
2019-09-05 15:39:27 +0900

    マイグレート

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190905034541) do
+ActiveRecord::Schema.define(version: 20190905063243) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "postalcode"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 20190905034541) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_brands_on_name", using: :btree
+  end
+
+  create_table "cards", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id",     null: false
+    t.integer  "customer_id", null: false
+    t.integer  "card_id",     null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_cards_on_user_id", using: :btree
   end
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -157,6 +166,7 @@ ActiveRecord::Schema.define(version: 20190905034541) do
     t.index ["user_id"], name: "index_users_products_on_user_id", using: :btree
   end
 
+  add_foreign_key "cards", "users"
   add_foreign_key "pictures", "products"
   add_foreign_key "products", "brands"
   add_foreign_key "products", "categories"
