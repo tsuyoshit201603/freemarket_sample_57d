2d745abb1311c7baf12f3cb413fda81769ca5f39
tsuyoshit201603 <tsuyoshi.t201603@gmail.com>
2019-09-06 14:09:08 +0900

    payingsコントローラにpayjpに関するアクションを記述

--- app/controllers/payings_controller.rb
+++ app/controllers/payings_controller.rb
@@ -1,7 +1,49 @@
 class PayingsController < TelphonesController
+  require "payjp"
   def new
     @active = ["is-active","is-active","is-active",""]
     @year = [*19..30]
     @month = [*1..12]
+    card = Card.where(user_id: current_user.id)
+    redirect_to action: "show" if card.exists?
+  end
+
+  def create
+    Payjp.api_key = Rails.application.secrets.payjp_private_key
+    if params['payjp-token'].blank?
+      redirect_to action: "new"
+    else
+      customer = Payjp::Customer.create(
+      card: params['payjp-token'],
+      @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      if @card.save
+        redirect_to action: "show"
+      else
+        redirect_to action: "pay"
+      end
+    end
+  end
+
+  def destroy
+    card = Card.where(user_id: current_user.id).first
+    if card.blank?
+    else
+      Payjp.api_key = Rails.application.secrets.payjp_private_key
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      customer.delete
+      card.delete
+    end
+      redirect_to action: "new"
+  end
+
+  def show
+    card = Card.where(user_id: current_user.id).first
+    if card.blank?
+      redirect_to action: "new" 
+    else
+      Payjp.api_key = Rails.application.secrets.payjp_private_key
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      @default_card_information = customer.cards.retrieve(card.card_id)
+    end
   end
 end
