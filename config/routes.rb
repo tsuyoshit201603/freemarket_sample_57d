58e22ca8df535fb039cdb79aa6fd168d7eed2bb7
tsuyoshit201603 <tsuyoshi.t201603@gmail.com>
2019-09-06 14:42:25 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,13 @@ Rails.application.routes.draw do
   resources :users, only: [:show] do
     resources :identifications, only: [:index]
     resources :profiles, only:[:new]
+    resources :payings, only:[:new,:create,:destroy,:show]
   end
   resources :logouts, only:[:destroy]
   resources :signups, only:[:index]
   resources :finishings, only:[:index]
   resources :telphones, only:[:new,:create]
   resources :addresses, only:[:new,:create]
-  resources :payings, only:[:new,:create,:destroy,:show]
   resources :payments, only:[:new,:create]
   resources :credits, only: [:new]
 end
