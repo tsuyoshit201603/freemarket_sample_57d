c78fd397df6d9cd9120424b4700b0089d25a058f
JUN1113 <dunkelrots@gmail.com>
2019-09-08 13:53:00 +0900

    DOMを定義

--- app/assets/javascripts/products/new.js
+++ app/assets/javascripts/products/new.js
@@ -1,2 +1,8 @@
 $(function(){
+  var fileArea = $(".main__product__item__h3");
+  var context = $(".main__product__item__h3__context");
+  var file = $(".main__product__item__h3__image");
+  var list = [];
+  list[0] = $("#uploadedListFirst");
+  list[1] = $("#uploadedListFinal");
 });
