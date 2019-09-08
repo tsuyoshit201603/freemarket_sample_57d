26b538ab7a1b4527221a09e30342bebcb55ebbb5
JUN1113 <dunkelrots@gmail.com>
2019-09-08 13:54:40 +0900

    サムネイルを差し込むfunction

--- app/assets/javascripts/products/new.js
+++ app/assets/javascripts/products/new.js
@@ -8,4 +8,23 @@ $(function(){
   var num = 0;
   var arrayNum = 0;
 
+  function appendHTML(url,arrayNum,num){
+    var html = `
+    <li class= "uploadedItem" data-num = "${num}">
+      <figure class = "uploadedItem__figure">
+        <img src ="${url}">
+      </figure>
+      <div class = "uploadedItem__buttons">
+        <div class = "uploadedItem__buttons__edit">
+          <a href >編集</a>
+        </div>
+        <div class = "uploadedItem__buttons__delete">
+          <a href >削除</a>
+        </div>
+      </div>
+    </li>
+    `
+    list[arrayNum].append(html);
+  }
+
 });
