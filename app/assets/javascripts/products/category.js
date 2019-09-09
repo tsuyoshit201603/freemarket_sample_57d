4672d14de2f8e5760a4b0cad872f617e0f8d551b
JUN1113 <dunkelrots@gmail.com>
2019-09-09 14:17:48 +0900

    appendHTML関数

--- app/assets/javascripts/products/category.js
+++ app/assets/javascripts/products/category.js
@@ -4,4 +4,17 @@ $(function(){
   selectbox[1] = $(".ancestry__child");
   selectbox[2] = $(".ancestry__grandChild");
   var url = "/api/products";
+  function appendHTML(position,data){
+    var html =`
+    <div class="main__product__details__h2-choice">
+      <select name="product[category_id]" id="product_category_id">
+        <option value = "">---</option>
+        ${data}
+      </select>
+      <i class="fa fa-chevron-down main__product__delivery__sell-box__icon"></i>
+    </div>
+    `
+    position.append(html);
+  }
+
 })
