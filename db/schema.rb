1f6ead7d6b4c7d0ad9f225dc0c29f58a664a631d
JUN1113 <dunkelrots@gmail.com>
2019-09-09 13:59:55 +0900

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190907054715) do
+ActiveRecord::Schema.define(version: 20190909043254) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "postalcode"
@@ -78,10 +78,10 @@ ActiveRecord::Schema.define(version: 20190907054715) do
   end
 
   create_table "pictures", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.text     "image",      limit: 65535, null: false
-    t.integer  "product_id",               null: false
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.integer  "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "image"
     t.index ["product_id"], name: "index_pictures_on_product_id", using: :btree
   end
 
