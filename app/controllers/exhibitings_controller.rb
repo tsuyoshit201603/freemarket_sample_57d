0ad6ca2350a9a262eafa7b603fe82c0bbd81b89b
tsuyoshit201603 <tsuyoshi.t201603@gmail.com>
2019-09-09 21:09:58 +0900

    出品した商品のビュー作成

--- app/controllers/exhibitings_controller.rb
+++ app/controllers/exhibitings_controller.rb
@@ -1,10 +1,29 @@
 class ExhibitingsController < ApplicationController
   def exhibiting
+    search_products(1)
   end
 
   def trading
+    search_products(2)
   end
 
   def sold
+    search_products(3)
+  end
+
+  private
+
+  def search_products(num)
+    products = Product.where(user_id: params[:user_id])
+    if products.present?
+      @products = products
+      if products.where(condition_id: num).present?
+        @products = products.where(condition_id: num)
+      else
+        @products = nil
+      end
+    else
+      @products = nil
+    end
   end
 end
