f9895d431487e6422f631104d58534b3c50bed14
JUN1113 <dunkelrots@gmail.com>
2019-09-09 14:17:23 +0900

    変数を定義

--- app/assets/javascripts/products/category.js
+++ app/assets/javascripts/products/category.js
@@ -1,2 +1,7 @@
 $(function(){
+  var selectbox = [];
+  selectbox[0] = $(".ancestry__parent");
+  selectbox[1] = $(".ancestry__child");
+  selectbox[2] = $(".ancestry__grandChild");
+  var url = "/api/products";
 })
