6f9b0d1fa556ba66dd2a079be1e3ad799923ee9d
JUN1113 <dunkelrots@gmail.com>
2019-09-12 13:10:57 +0900

    変数の定義

--- app/assets/javascripts/products/thumbnail.js
+++ app/assets/javascripts/products/thumbnail.js
@@ -1,2 +1,13 @@
 $(function(){
+  var fileArea = $(".main__product__item__h3");
+  var context = $(".main__product__item__h3__context");
+  var file = $(".main__product__item__h3__image");
+  var list = [];
+  list[0] = $("#uploadedListFirst");
+  list[1] = $("#uploadedListFinal");
+  var num = 0;
+  var listNum = 0;
+  var arrayNum = 0;
+  var fileArray = [];
+  var deleteIDArray = [];
 });
