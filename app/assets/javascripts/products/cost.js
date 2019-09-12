0fabe165a81d95f85e32a5a63c0745cb7064c147
JUN1113 <dunkelrots@gmail.com>
2019-09-12 13:03:13 +0900

    コスト計算をthumbnailからcostに分離

---
+++ app/assets/javascripts/products/cost.js
@@ -0,0 +1,16 @@
+$(function(){
+  var cost = $(".main__product__price__h2-choice");
+
+  // 手数料と利益の計算を行う
+  cost.on("keyup", function(){
+    var input = $(this).val();
+    if(input >= 300 && input <= 9999999){
+      $(".main__product__price__h3__cost").html(`￥${(Math.round(input * 0.1)).toLocaleString()}`);
+      $(".main__product__price__h4__profit").html(`￥${(Math.round(input * 0.9)).toLocaleString()}`);
+    }
+    else{
+      $(".main__product__price__h3__cost").html(`-`);
+      $(".main__product__price__h4__profit").html("-");
+    }
+  });
+});
