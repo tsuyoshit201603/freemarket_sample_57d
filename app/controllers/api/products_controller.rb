8d3a04e6089ea42493b4da38bc73c1d20899dff9
JUN1113 <dunkelrots@gmail.com>
2019-09-12 13:03:53 +0900

    サムネイルを表示するためのAPIを作成

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -2,4 +2,7 @@ class Api::ProductsController < ApplicationController
   def index
     @childrenCategories = Category.find_by(id: params[:selected])&.children
   end
+  def edit
+    @pictures = Product.find(params[:id]).pictures
+  end
 end
