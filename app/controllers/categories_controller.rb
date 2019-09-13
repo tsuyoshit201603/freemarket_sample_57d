d24f71b4a1ad678912d140e50b189508cabecf26
JUN1113 <dunkelrots@gmail.com>
2019-09-13 13:11:08 +0900

    showアクションを設定

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,14 @@
 class CategoriesController < ApplicationController
+  def show
+    @category = Category.find(params[:id])
+    if @category.ancestry
+      if @category.children.present?
+        @family = "child"
+      else
+        @family = "grandchild"
+      end
+    else
+      @family = "parent"
+    end
+  end
 end
